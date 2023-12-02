bc1e47fbd41ae7a8f627901b88600bd857fd8cc3
Ebaghrar <117086855+Ebaghrar@users.noreply.github.com>
2023-12-03 00:58:05 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from PIL import Image, ImageDraw
+import os
+
+def create_image():
+    # Créer une image simple
+    image = Image.new("RGB", (300, 300), "white")
+    draw = ImageDraw.Draw(image)
+
+    # Dessiner un rectangle rouge
+    draw.rectangle([50, 50, 250, 250], outline="red", width=5)
+
+    # Enregistrer l'image
+    image_path = os.path.join(os.getcwd(), "output_image.png")
+    image.save(image_path)
+    return image_path
+
+if __name__ == "__main__":
+    image_path = create_image()
+    print(f"Image créée avec succès : {image_path}")
